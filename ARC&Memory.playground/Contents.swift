807a64185d918831c5db03f3408a7469611de235
Jayfunf <simh3077@gmail.com>
2022-07-18 06:08:21 +0900

    modify

--- ARC&Memory.playground/Contents.swift
+++ ARC&Memory.playground/Contents.swift
@@ -8,9 +8,22 @@ class Human {
     init(name: String?, age: Int?) {
         self.name = name
         self.age = age
+        print("\(name!) is being initialized")
+    }
+    
+    deinit {
+        print("\(name!) is being deinitialized")
     }
 }
  
-let Minion = Human(name: "Minion", age: 24)
+var reference1: Human?
+var reference2: Human?
+var reference3: Human?
 
+reference1 = Human(name: "Minion", age: 24)
+reference2 = reference1
+reference3 = reference1
 
+reference1 = nil
+reference2 = nil
+reference3 = nil

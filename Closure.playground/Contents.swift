98283c294397e60d4349b742899c04807a01554c
Jayfunf <simh3077@gmail.com>
2022-08-05 20:20:14 +0900

    modify

--- Closure.playground/Contents.swift
+++ Closure.playground/Contents.swift
@@ -1,4 +1,6 @@
 import UIKit
+import os
+import Foundation
 
 //func helloGenerator(message: String) -> (String, String) -> String {
 //    return { ( firstName: String , lastName: String) -> String in
@@ -18,12 +20,88 @@ import UIKit
 //    }
 //}
 
-func helloGenerator(message: String) -> (String, String) -> String {
-    return { $1 + $0 + message }
+//func helloGenerator(message: String) -> (String, String) -> String {
+//    return { $1 + $0 + message }
+//}
+//
+//let someVariable = helloGenerator(message: "님 어서오세요.")
+//print(someVariable("Minhyun ", "Cho "))
+//
+//var closureVariable: (String, String) -> String = { $1 + $0 + "님 어서오세요."}
+//print(closureVariable("Minhyun ", "Cho "))
+
+//let closure = { (name: String) -> String in
+//    return "Hello, \(name)"
+//}
+//
+//let hello = { () -> () in
+//    print("Hello")
+//}
+//
+//let hello2 = hello
+//
+//closure("Minion")
+//hello()
+//hello2()
+//
+//func doSome() -> () -> () {
+//    return { () -> () in
+//        print("Hello")
+//    }
+//}
+//
+//doSome()
+//let DO = doSome()
+//
+//DO()
+//
+//let someClosure: (String) -> String
+//
+//someClosure = { (name: String) in
+//    return "Hello, \(name)"
+//}
+//
+//someClosure("Minion")
+
+//MARK: Closure
+let helloClosure = { (name: String) -> String in
+    return "Hello, \(name)"
+}
+helloClosure("Minion")
+
+//MARK: Function
+func helloFunction(name: String) -> String {
+    return "Hello, \(name)"
+}
+helloFunction(name: "Minion")
+
+let helloPrintClosure = { (name: String) in
+    print("Hello, \(name)")
 }
 
-let someVariable = helloGenerator(message: "님 어서오세요.")
-print(someVariable("Minhyun ", "Cho "))
+func doSomeThing(name: String, closure: (String) -> ()){
+    closure(name)
+}
+doSomeThing(name: "Minion", closure: helloPrintClosure)
 
-var closureVariable: (String, String) -> String = { $1 + $0 + "님 어서오세요."}
-print(closureVariable("Minhyun ", "Cho "))
+func doSomeThing2(closure: () -> ()){
+    closure()
+}
+doSomeThing2(closure: {
+    print("Hello")
+})
+
+let normalClosure = { () -> Void in
+    print("Hello, closureInClosure")
+}
+let closureInClosure = { (closure: () -> ()) -> Void in
+    return closure()
+}
+closureInClosure(normalClosure)
+
+//MARK: 일회용 클로저
+({ () -> () in
+    print("Hello Sodeul!")
+})()
+
+

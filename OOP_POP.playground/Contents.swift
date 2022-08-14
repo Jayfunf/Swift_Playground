c062abb18b4aead95fd7654492fabf3973af1a3b
Jayfunf <simh3077@gmail.com>
2022-08-14 16:47:03 +0900

    init

---
+++ OOP_POP.playground/Contents.swift
@@ -0,0 +1,58 @@
+import Foundation
+
+//struct Power {
+//    func power() {
+//        print("Power UP!!")
+//    }
+//}
+//
+//struct FastRun {
+//    func fastRun() {
+//        print("Faster than car!!")
+//    }
+//}
+//
+//class superman {
+//    private let powerup = Power()
+//    private let speedup = FastRun()
+//
+//    func power() {
+//        powerup.power()
+//    }
+//
+//    func speed() {
+//        speedup.fastRun()
+//    }
+//}
+//
+//let Superman = superman()
+//Superman.power()
+//Superman.speed()
+
+protocol Power {
+    func power()
+}
+
+extension Power {
+    func power() {
+        print("PowerUP Activate")
+    }
+}
+
+protocol FastRun {
+    func fastRun()
+}
+
+extension FastRun {
+    func fastRun() {
+        print("FastRun Activate")
+    }
+}
+
+class Superman: Power, FastRun {
+    
+}
+
+let superman = Superman()
+superman.power()
+superman.fastRun()

8d497675ef859afe81dbbb420ec9549f1d244157
Jayfunf <simh3077@gmail.com>
2022-09-01 22:37:10 +0900

    waited

--- Associatedtype.playground/Contents.swift
+++ Associatedtype.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var greeting = "Hello, playground"
+protocol Animal {
+    associatedtype MyType
+    
+    var name: MyType { get }
+}
+
+struct dog: Animal {
+    var name: String {
+        return "Dog"
+    }
+}
+
+struct bird: Animal {
+    var name: Int {
+        return 10
+    }
+}
+
+struct cat: Animal {
+    var name: Character {
+        return "A"
+    }
+}
+
+var test = dog()
+
+var test2 = bird()
+
+var test3 = cat()
+
+print(test.name)
+
+print(test2.name)
+
+print(test3.name)

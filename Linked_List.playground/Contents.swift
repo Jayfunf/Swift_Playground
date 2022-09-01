44f9b3e8217ac25b92230534256f9ee28f17da0c
Jayfunf <simh3077@gmail.com>
2022-09-02 03:57:36 +0900

    modify: LinkedList

--- Linked_List.playground/Contents.swift
+++ Linked_List.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class Node<T> {
+    var data: T?
+    var next: Node?
+    
+    init(data: T?, next: Node? = nil) {
+        self.data = data
+        self.next = next
+    }
+}
+
+class LinkedList<T> {
+    private var head: Node<T>?
+    
+    func append(data: T?) {
+        if head == nil {
+            head = Node(data: data)
+            return
+        }
+        
+        var node = head
+        while node?.next != nil {
+            node = node?.next
+        }
+        node?.next = Node(data: data)
+    }
+}

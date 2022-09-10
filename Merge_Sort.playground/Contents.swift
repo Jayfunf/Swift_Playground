8e66ab3e12d3815af16ef2bea36b73abf6a019fe
Jayfunf <simh3077@gmail.com>
2022-09-10 23:50:15 +0900

    modify

--- Merge_Sort.playground/Contents.swift
+++ Merge_Sort.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 var arr: [Int] = [7,9,2,5,6,1]
 
 func mergeSort(_ arr: [Int]) -> [Int] {
-    if arr.count <= 1 { return arr }
+    if arr.count <= 1 { return arr } // 배열이 다 쪼개질 때까지 반복. 만약 리턴받은 배열의 크기가 1보다 작거나 같다면 divide종료.
     let center = arr.count / 2
     let left = Array(arr[0..<center])
     let right = Array(arr[center..<arr.count])

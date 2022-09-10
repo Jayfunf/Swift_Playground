8059f10e756fcc497336a7f827a1f9c04ea1afb6
Jayfunf <simh3077@gmail.com>
2022-09-10 22:53:58 +0900

    modify

--- Merge_Sort.playground/Contents.swift
+++ Merge_Sort.playground/Contents.swift
@@ -3,23 +3,34 @@ import UIKit
 var arr: [Int] = [7,9,2,5,6,1]
 
 func mergeSort(_ arr: [Int]) -> [Int] {
-    var center = arr.count / 2
-    var left = Array(arr[0..<center])
-    var right = Array(arr[center..<arr.count])
-    
-    print(left)
-    print(right)
-    
-    func merge(left: [Int], right: [Int]) -> [Int] {
+    if arr.count <= 1 { return arr }
+    let center = arr.count / 2
+    let left = Array(arr[0..<center])
+    let right = Array(arr[center..<arr.count])
+
+    func merge(_ left: [Int], _ right: [Int]) -> [Int] {
         var left = left
         var right = right
-        var reult: [Int] = []
+        var result: [Int] = []
+
+        while !left.isEmpty && !right.isEmpty {
+            if left[0] < right[0] {
+                result.append(left.removeFirst())
+            } else {
+                result.append(right.removeFirst())
+            }
+        }
         
+        if !left.isEmpty {
+            result.append(contentsOf: left)
+        }
         
-        
+        if !right.isEmpty {
+            result.append(contentsOf: right)
+        }
+        return result
     }
-    
-    return merge(left: mergeSort(left), right: mergeSort(right))
+    return merge(mergeSort(left), mergeSort(right))
 }
 
-mergeSort(arr)
+print(mergeSort(arr))

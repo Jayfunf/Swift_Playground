777d926c3c7bcd1fd3dd0b7d2b4bee05899fbc82
Jayfunf <simh3077@gmail.com>
2022-09-17 21:04:21 +0900

    modify

--- DFS&BFS.playground/Contents.swift
+++ DFS&BFS.playground/Contents.swift
@@ -12,7 +12,7 @@ let adjacencyMatrix = [ // Adjacency Matrix
     [7]     //8
 ]
 
-let adjacencyList:[Int: [Int]] = [ // Adjacency List
+let adjacencyList:[Int: [Int]] = [ // Adjacency List structed by Dictionary
     0:[0],
     1:[2,3],
     2:[1,4,5],
@@ -24,51 +24,49 @@ let adjacencyList:[Int: [Int]] = [ // Adjacency List
     8:[7]
 ]
 
-class DFS {
-    var recul_visited = Array.init(repeating: false, count: adjacencyMatrix.count)
-    var recul_visited2 = Array.init(repeating: false, count: adjacencyMatrix.count)
-    var normal_visited = Array.init(repeating: 0, count: adjacencyList.count)
-    var normal_visited2 = Array.init(repeating: 0, count: adjacencyList.count)
+var recul_Matrix = Array.init(repeating: false, count: adjacencyMatrix.count)
+var recul_List = Array.init(repeating: false, count: adjacencyList.count)
+
+func reculsiveDFS(_ startNode: Int) { // Complete
+    //recul_Matrix[startNode] = true // Matrix
+    recul_List[startNode] = true // List
     
-    func reculsiveMatrix(_ startNode: Int) { // Complete
-        recul_visited[startNode] = true
-        
-        print(startNode, terminator: " ")
-        
-        for i in adjacencyMatrix[startNode] {
-            if !recul_visited[i] {
-                reculsiveMatrix(i)
-            }
+    //print(startNode, terminator: " ")
+    
+//    for i in adjacencyMatrix[startNode] { // Matrix
+//        if !recul_Matrix[i] {
+//            reculsiveDFS(i)
+//        }
+//    }
+    
+    for i in adjacencyList[startNode]! { // List
+        print("List: ", adjacencyList[startNode]!)
+        print("stratNode: ", startNode)
+        print("i: ", i)
+        print("recul_List[i]: ", !recul_List[i])
+        if !recul_List[i] { // false이면 동작
+            print("reculsive")
+            reculsiveDFS(i)
         }
     }
-    
-    func reculsiveList(_ startNode: Int) { // Complete
-        recul_visited2[startNode] = true
-        
-        print(startNode, terminator: " ")
-        
-        for i in adjacencyList[startNode]! {
-            if !recul_visited2[i] {
-                reculsiveList(i)
-            }
-        }
-    }
-    
-    func normalMatrix() {
-        
-    }
-    
-    func normalList() {
-        
-    }
-    
-}
+} // 1 2 4 5 3 6 7 8
 
-struct BFS {
-    
-}
+reculsiveDFS(1)
 
-var test = DFS()
-test.reculsiveMatrix(1)
+func normalDFS(graph: [Int: [Int]], start: Int) -> [Int] {
+    var visitedQueue: [Int] = []
+    var needVisitStack: [Int] = [start]
+    
+    while !needVisitStack.isEmpty {
+        let node: Int = needVisitStack.removeLast()
+        if visitedQueue.contains(node) { continue }
+        
+        visitedQueue.append(node)
+        needVisitStack += graph[node] ?? []
+    }
+    return visitedQueue
+} // [1, 3, 7, 8, 6, 2, 5, 4]
+
+
 print("\n")
-test.reculsiveList(1)
+print(normalDFS(graph: adjacencyList, start: 1))

147ddccd498fe7fb87428332f0bbade1060f7b02
Jayfunf <simh3077@gmail.com>
2022-09-28 01:31:42 +0900

    modify

--- Dictionary.playground/Contents.swift
+++ Dictionary.playground/Contents.swift
@@ -1,19 +1,40 @@
 import UIKit
 
-var dic: [Int: String] = [:]
+//var dic: [Int: String] = [:]
+//
+//var dic2: [String: Int] = [:]
+//
+//var dic3: [String: [String]] = [:]
+//
+//dic.updateValue("Test1", forKey: 0)
+//dic.updateValue("Test2", forKey: 1)
+//
+//dic2.updateValue(10, forKey: "1")
+//dic2.updateValue(20, forKey: "2")
+//
+//dic3.updateValue(["test1"], forKey: "1")
+//dic3.updateValue(["test2"], forKey: "2")
+//dic3.updateValue(["test3"], forKey: "2")
 
-var dic2: [String: Int] = [:]
+//print(dic3)
 
-var dic3: [String: [String]] = [:]
+//var dic: [Int: Int] = [:]
+//
+//dic.updateValue(100, forKey: 1)
+//dic.updateValue(90, forKey: 2)
+//dic.updateValue(80, forKey: 3)
+//dic.updateValue(70, forKey: 4)
+//dic.updateValue(60, forKey: 5)
+//
+//print(dic)
+//
+//print(dic[1])
 
-dic.updateValue("Test1", forKey: 0)
-dic.updateValue("Test2", forKey: 1)
-
-dic2.updateValue(10, forKey: "1")
-dic2.updateValue(20, forKey: "2")
-
-dic3.updateValue(["test1"], forKey: "1")
-dic3.updateValue(["test2"], forKey: "2")
-dic3.updateValue(["test3"], forKey: "2")
-
-print(dic3)
+var i: [Int] = [1,2,3,4,5]
+print(i.isEmpty)
+print(i.count)
+while !i.isEmpty {
+    print(i)
+    i.popLast()
+    
+}
